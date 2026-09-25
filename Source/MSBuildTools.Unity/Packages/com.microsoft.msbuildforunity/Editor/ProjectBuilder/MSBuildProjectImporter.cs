9dc61902cd92cdc2a361aa13809668775aba6b8a
Urs Hanselmann <git@hanselmann.io>
2026-09-25 23:59:33 +0200

    use UnityEditor.AssetImporters on Unity 2020.2+ instead of the removed experimental namespace

--- Source/MSBuildTools.Unity/Packages/com.microsoft.msbuildforunity/Editor/ProjectBuilder/MSBuildProjectImporter.cs
+++ Source/MSBuildTools.Unity/Packages/com.microsoft.msbuildforunity/Editor/ProjectBuilder/MSBuildProjectImporter.cs
@@ -2,7 +2,11 @@
 using System.IO;
 using System.Linq;
 using UnityEditor;
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 using UnityEngine;
 
 namespace Microsoft.Build.Unity
